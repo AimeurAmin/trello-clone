edaeb399d2078d14c65558d9fddad9288b266ce5
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-06-30 12:15:41 +0100

    Add item button

---
+++ src/components/AddItemButton/AddItemButton.tsx
@@ -0,0 +1,21 @@
+import styles from "./AddItemButton.module.scss";
+import { FC } from "react";
+
+interface AddItemButtonProps {
+  dark: boolean;
+  onClick(): void;
+  children?: React.ReactNode;
+}
+
+const AddItemButton = ({ dark, onClick }: AddItemButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles["container"]} ${styles[dark ? "dark" : "notDark"]}`}
+    >
+      {`+ Add another ${dark ? "task" : "list"}`}
+    </button>
+  );
+};
+
+export default AddItemButton;
