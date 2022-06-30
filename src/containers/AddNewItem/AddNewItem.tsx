e436d60f1f7e689a700ea702669f8990cfa1b375
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-06-30 12:18:16 +0100

    Adds new item container

---
+++ src/containers/AddNewItem/AddNewItem.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import AddItemButton from "../../components/AddItemButton/AddItemButton";
+import NewItemForm from "../../components/NewItemForm/NewItemForm";
+
+interface AddNewItemProps {
+  onAdd(text: string): void;
+  toggleButtonText: string;
+  dark: boolean;
+}
+
+const AddNewItem = ({ onAdd, toggleButtonText, dark }: AddNewItemProps) => {
+  const [showForm, setShowForm] = useState<boolean>(false);
+
+  if (showForm) {
+    return (
+      <NewItemForm
+        onAdd={(text) => {
+          onAdd(text);
+          setShowForm(false);
+        }}
+      />
+    );
+  }
+  return (
+    <div>
+      <AddItemButton dark={dark} onClick={() => setShowForm(true)}>
+        {toggleButtonText}
+      </AddItemButton>
+    </div>
+  );
+};
+
+export default AddNewItem;
