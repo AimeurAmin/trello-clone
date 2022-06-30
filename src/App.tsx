31734bd8d685f65450d53cbadfb4d544e04485a8
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-06-30 12:18:43 +0100

    putting it all together in main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { FC, PropsWithChildren } from "react";
 import styles from "./App.module.scss";
+import AddItemButton from "./components/AddItemButton/AddItemButton";
+import Card from "./components/Card/Card";
+import Columns from "./components/Columns/Columns";
+import AddNewItem from "./containers/AddNewItem/AddNewItem";
 
-function App() {
-  return <div className={styles.container}>Trello clone</div>;
-}
+const App: React.FC<any> = ({ color, children }) => {
+  return (
+    <div className={styles["container"]}>
+      <Columns text="Backlog">
+        <Card text="Init trello project" />
+        <Card text="Set global styles" />
+      </Columns>
+
+      <Columns text="In progress">
+        <Card text="Learn React TS" />
+      </Columns>
+
+      <Columns text="Done">
+        <Card text="Learn Typescript" />
+        <Card text="Learn React (JS)" />
+      </Columns>
+
+      <AddNewItem
+        dark={false}
+        toggleButtonText="+ Add another list"
+        onAdd={console.log}
+      />
+    </div>
+  );
+};
 
 export default App;
