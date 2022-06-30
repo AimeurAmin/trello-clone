03b2aca45d71c4eefd3ca3657aba72f5c7c51912
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-06-30 12:14:16 +0100

    unused test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello React typescript/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("render cool", () => {
-  render(<App />);
-  const elmnt = screen.getByTestId("custom-element");
-  expect(elmnt).toBeInTheDocument();
-});
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Hello React typescript/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
